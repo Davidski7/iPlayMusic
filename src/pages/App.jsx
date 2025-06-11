74970827bae35c62bd2ef864e9bfdae4d0f9362a
Davidski7 <U2486889@rts-365.dk>
2025-06-11 09:19:25 +0200

    Min start

---
+++ src/pages/App.jsx
@@ -0,0 +1,16 @@
+import SplashScreen from '../componets/splashscreen'
+
+function App() {
+
+
+  return (
+    <>
+      <SplashScreen />
+      <div>
+        <h1>Het!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
